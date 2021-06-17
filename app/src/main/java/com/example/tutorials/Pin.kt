11c0ad6574268eb5a5043625ba8d374a9d51bf48
Nafi-13 <nafirazzaque@gmail.com>
2021-06-17 12:15:22 +0600

    Download(Incomplete)

--- app/src/main/java/com/example/tutorials/Pin.kt
+++ app/src/main/java/com/example/tutorials/Pin.kt
@@ -4,4 +4,5 @@ import kotlin.random.Random.Default.nextInt
 
 data class Pin(val name: String, val likes: Int) {
     var pinUrl = "https://picsum.photos/${nextInt(200,500)}/${nextInt(200,500)}"
+
 }
